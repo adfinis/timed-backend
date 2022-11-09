1c8f5c5e57b8ec25621625377fa2aa6b2927ff03
Wiktor Kepczynski <55092761+trowik@users.noreply.github.com>
2022-11-09 13:59:55 +0100

    Revert "chore: prevent poetry from creating virtual environment"

--- cmd.sh
+++ cmd.sh
@@ -4,10 +4,10 @@
 
 set -x
 
-./manage.py manage.py collectstatic --noinput
+poetry run python manage.py manage.py collectstatic --noinput
 
 set -e
 
 wait-for-it.sh "${DJANGO_DATABASE_HOST}":"${DJANGO_DATABASE_PORT}" -t "${WAITFORIT_TIMEOUT}"
-./manage.py migrate --no-input
-./manage.py serve --static --port 80 --req-queue-len "${HURRICANE_REQ_QUEUE_LEN:-250}" "$@"
+poetry run python manage.py migrate --no-input
+poetry run python manage.py serve --static --port 80 --req-queue-len "${HURRICANE_REQ_QUEUE_LEN:-250}" "$@"
